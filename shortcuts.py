b67a26d70eccfc2dde0f19f2b040c6d63cd5d062
Jeff Aigner <enumeration@gmail.com>
2012-01-24 21:02:51 -0600

    Cleaned up a bit

--- shortcuts.py
+++ shortcuts.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+# a shorter form of render_to_response
 rtr = lambda x,y,z: render_to_response(x, y, context_instance=RequestContext(z))
 
--- urls.py
+++ urls.py
@@ -5,13 +5,8 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'solidstate.views.home', name='home'),
-    # url(r'^solidstate/', include('solidstate.foo.urls')),
+    url(r'^$', 'solidstate.blog.views.home'),
     url(r'^blog/', include('solidstate.blog.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
