e63c6edbe0509da415b9b6cd356737241fa8d6f9
Jeff Aigner <enumeration@gmail.com>
2012-01-24 20:55:34 -0600

    Added django-extensions

--- settings.py
+++ settings.py
@@ -116,8 +116,14 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'solidstate.blog',
+
+    # 3rd party extensions
     'south',
+    'django_extensions',
+
+    # Solid State Apps
+    'solidstate.blog',
+
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
